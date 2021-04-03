507b32eef7e6c57815f58cee60ab32540755f61c
elisa <elisaopalka@gmail.com>
2021-04-03 15:41:13 -0400

    added in redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import gamesReducer from './reducers/gamesReducer';
+
+const store = createStore(gamesReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
