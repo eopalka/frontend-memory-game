9389680768415d1b063c47f6a1335f804dc03741
elisa <elisaopalka@gmail.com>
2021-04-03 21:05:48 -0400

    started form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
 
-export class Form extends Component {
+class Form extends Component {
+
+    state = {
+        username: ""
+    }
+
+    handleChange = e => {
+        this.setState({
+          [e.target.name]: e.target.value
+        })
+      }
+    
+      handleSubmit = e => {
+        e.preventDefault();
+    
+        this.props.addGame(this.state, this.props.history);
+      }
+
     render() {
         return (
             <div>
-                Form
+                <h3>Who is You?</h3>
+                <form onSubmit={ this.handleSubmit }>
+                    <input type="text" id="username" name="username" value={ this.state.username } onChange={ this.handleChange } />
+                    <input type="submit" value="Enter" />
+                </form>
             </div>
         )
     }
 }
 
-export default Form
+export default connect(null, { addGame })(Form)
