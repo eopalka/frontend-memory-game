77107e3bdae156bff44a2001e9ce432f74203d3a
elisa <elisaopalka@gmail.com>
2021-04-03 21:10:42 -0400

    set up navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 export class Navbar extends Component {
     render() {
         return (
             <div>
-                Nav
+                <ul>
+                    <li><Link to="/">Welcome</Link></li>
+                    <li><Link to="/instructions">Instructions</Link></li>
+                </ul>
             </div>
         )
     }
