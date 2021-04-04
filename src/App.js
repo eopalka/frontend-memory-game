e23315a0a5667c2d65ed9230306d09e7fbabb257
elisa <elisaopalka@gmail.com>
2021-04-03 21:05:14 -0400

    imported components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 // import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { BrowserRouter as  Router, Switch, Route } from 'react-router-dom';
+import { getGames } from './actions';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Welcome from './components/Welcome';
+import ErrorPage from './components/Error';
+import Instructions from './components/Instructions';
+import Index from './components/Index'
+
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.getGames();
+  }
+
   render() {
+    if (this.props.loading) {
+      return (
+        <h3>loading</h3>
+      )
+    }
+
   return (
-    <div className="App">
-     <h1>Hello Bitches</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ Welcome } />
+        <Route exact path="/instructions" component={ Instructions } />
+        <Route exact path="/games" component={ Index } />
+        <Route component={ErrorPage} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    loading: state.loading
+  }
+}
+
+export default connect(mapStateToProps, { getGames })(App);
+
