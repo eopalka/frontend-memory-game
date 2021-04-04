37ca6b27e3a70da94cb202cfe826fd446d86344d
elisa <elisaopalka@gmail.com>
2021-04-03 21:09:15 -0400

    first draft welcome page

---
+++ src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Welcome extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello Bitches</h1>
+            </div>
+        )
+    }
+}
+
+export default Welcome
