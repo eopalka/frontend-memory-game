6a35cc9eed9b11153195e475ddb51bd1df036d50
elisa <elisaopalka@gmail.com>
2021-04-03 21:11:16 -0400

    set up index page

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import Game from './Game';
 
-export class Index extends Component {
+class Index extends Component {
     render() {
+        const games = this.props.games.map( (game, i) => <Game key={i} timer={ game.timer } moves={ game.moves } /> )
         return (
             <div>
-                List Games Page
+                { games }
             </div>
         )
     }
 }
 
-export default Index
+const mapStateToProps = state => {
+    return {
+        games: state.games
+    }
+}
+
+export default connect(mapStateToProps)(Index);
