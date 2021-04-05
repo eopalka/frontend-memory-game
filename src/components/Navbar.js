8564a6b4ccf3c50be620fcc2ae8fded38bca799f
elisa <elisaopalka@gmail.com>
2021-04-04 23:50:08 -0400

    play game and scoreboard links to nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,8 @@ export class Navbar extends Component {
                 <ul>
                     <li><Link to="/">Welcome</Link></li>
                     <li><Link to="/instructions">Instructions</Link></li>
+                    <li><Link to="/game">Play Game</Link></li>
+                    <li><Link to="/scoreboard">Score Board</Link></li>
                 </ul>
             </div>
         )
