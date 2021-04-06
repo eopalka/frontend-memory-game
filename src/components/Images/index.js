dde2ca21a209c50d8f68ffb8c562ee9053d1e6fb
elisa <elisaopalka@gmail.com>
2021-04-06 15:32:50 -0400

    added card pictures

---
+++ src/components/Images/index.js
@@ -0,0 +1,36 @@
+import Henry from "./henry.jpg"
+import Walnut from "./walnut.PNG"
+import Bandit from "./bandit.jpg"
+import Icon from "./icon.png"
+
+
+
+
+export let images = [
+    {
+        name: "Henry", pic: Henry, flipped: false
+    },
+
+    {
+        name: "Walnut", pic: Walnut, flipped: false
+    },
+
+    {
+        name: "Bandit", pic: Bandit, flipped: false
+    },
+
+    {
+        name: "Henry", pic: Henry, flipped: false
+    },
+
+    {
+        name: "Walnut", pic: Walnut, flipped: false
+    },
+
+    {
+        name: "Bandit", pic: Bandit, flipped: false
+    },
+
+]
+
+export const blankCard = Icon
