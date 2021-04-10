a4f4a4af1996c27fd71c2ded92460fd1d9fb02a3
elisa <elisaopalka@gmail.com>
2021-04-09 23:35:10 -0400

    updated state of game over component

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addGame } from "../actions";
+import  history  from "../history"
 
-export class GameOver extends Component {
-    handleClick = () => {
-        this.props.StartGame(false)
-    }   
+class GameOver extends Component {
+
+    state = {
+        user_id: this.props.user.id,
+        timer: "",
+        moves: "",
+        user_attributes: this.props.user
+    }
+
+      handleSubmit = e => {
+        this.props.addGame(this.state, history);
+        this.props.handleModal(false)
+      }
+
+ 
         render() {
         return (
             <div className="game-over">
-        <button className="btn btn-primary" onClick={this.handleClick}>Play Again?</button>    
+                <button className="btn btn-primary" onClick={this.handleSubmit}>Good Job!</button>    
             </div>
         )
     }
 }
 
-export default GameOver
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    }
+  }
+
+export default connect(mapStateToProps, { addGame }) (GameOver)
