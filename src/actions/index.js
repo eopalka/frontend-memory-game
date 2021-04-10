4664d64504e2e338b4ba37c428db11c56391b69b
elisa <elisaopalka@gmail.com>
2021-04-09 23:40:00 -0400

    fetch for add game and add user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,6 @@
+export const currentUser = (user) => ({ type: "CREATE_USER", payload: user })
+export const updateGamesPlayed = (games_played) => ({ type: "UPDATE_GAMES_PLAYED", payload: games_played })
+
 export const getGames = () => {
     return dispatch => {
         dispatch({ type: "LOADING" })
@@ -5,4 +8,40 @@ export const getGames = () => {
             .then(resp => resp.json())
             .then(games => dispatch({ type:"SET_GAMES", games }))
     }
+}
+
+
+export const addGame = (game, history) => {
+    return dispatch => {
+        fetch("http://localhost:3001/games", {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify( game )
+      })
+        .then(resp => resp.json())
+        .then(game => {
+          dispatch({ type: "ADD_GAME", game })
+          history.push("/game")
+        })
+    }
+  }
+
+export const addUser = (user) => {
+  return dispatch => {
+    fetch("http://localhost:3001/users", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ user })
+  })
+    .then(resp => resp.json())
+    .then(userData => {
+      dispatch( currentUser(userData))
+    })
+  }
 }
