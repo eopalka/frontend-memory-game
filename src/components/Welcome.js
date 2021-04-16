3a1183c6358a08d51d9c83053b5a70e48940e48e
elisa <elisaopalka@gmail.com>
2021-04-16 13:39:36 -0400

    updated class name

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Welcome extends Component {
     render() {
         return (
-            <div className="App">
+            <div className= "login">
                 <h1>Time to test your memeory.</h1>
             </div>
         )
