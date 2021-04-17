db12fe5b55e38b2bdb917b97caa518a1bdbe31c1
elisa <elisaopalka@gmail.com>
2021-04-17 17:35:52 -0400

    added new pictures to cards

--- src/components/game/Images/index.js
+++ src/components/game/Images/index.js
@@ -1,7 +1,16 @@
 import Henry from "./henry.jpg"
-import Walnut from "./walnut.PNG"
-import Bandit from "./bandit.jpg"
+import banditt from "./banditt.jpg"
+import bbhen from "./bbhen.jpg"
+import bbhenhen from "./bbhenhen.jpg"
 import bbhenry from "./bbhenry.jpg"
+import bulky from "./bulky.PNG"
+import cat from "./cat.jpg"
+import chai from "./chai.PNG"
+import ginger from "./ginger.jpg"
+import hen from "./hen.jpg"
+import sink from "./sink.jpg"
+import walnutt from "./walnutt.jpg"
+
 import Icon from "./icon.png"
 
 
@@ -13,15 +22,47 @@ export let images = [
     },
 
     {
-        name: "Walnut", pic: Walnut, flipped: false
+        name: "banditt", pic: banditt, flipped: false
     },
 
     {
-        name: "Bandit", pic: Bandit, flipped: false
+        name: "bbhen", pic: bbhen, flipped: false
+    },
+
+    {
+        name: "bbhenhen", pic: bbhenhen, flipped: false
     },
 
     {
         name: "bbhenry", pic: bbhenry, flipped: false
+    },
+
+    {
+        name: "bulky", pic: bulky, flipped: false
+    },
+
+    {
+        name: "cat", pic: cat, flipped: false
+    },
+
+    {
+        name: "chai", pic: chai, flipped: false
+    },
+
+    {
+        name: "ginger", pic: ginger, flipped: false
+    },
+
+    {
+        name: "hen", pic: hen, flipped: false
+    },
+
+    {
+        name: "sink", pic: sink, flipped: false
+    },
+
+    {
+        name: "walnutt", pic: walnutt, flipped: false
     },
 
     {
@@ -29,17 +70,48 @@ export let images = [
     },
 
     {
-        name: "Walnut", pic: Walnut, flipped: false
+        name: "banditt", pic: banditt, flipped: false
     },
 
     {
-        name: "Bandit", pic: Bandit, flipped: false
+        name: "bbhen", pic: bbhen, flipped: false
+    },
+
+    {
+        name: "bbhenhen", pic: bbhenhen, flipped: false
     },
 
     {
         name: "bbhenry", pic: bbhenry, flipped: false
     },
 
+    {
+        name: "bulky", pic: bulky, flipped: false
+    },
+
+    {
+        name: "cat", pic: cat, flipped: false
+    },
+
+    {
+        name: "chai", pic: chai, flipped: false
+    },
+
+    {
+        name: "ginger", pic: ginger, flipped: false
+    },
+
+    {
+        name: "hen", pic: hen, flipped: false
+    },
+
+    {
+        name: "sink", pic: sink, flipped: false
+    },
+
+    {
+        name: "walnutt", pic: walnutt, flipped: false
+    },
 
 ]
 
